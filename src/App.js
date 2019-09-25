4e4b99bd03064fac353b351c039a2adffc0c971f
mihwa <mihwa@us.ibm.com>
2019-09-25 17:12:03 -0400

    git testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, I've been deployed to AWS!
+          Hi there!
         </p>
       </header>
     </div>
